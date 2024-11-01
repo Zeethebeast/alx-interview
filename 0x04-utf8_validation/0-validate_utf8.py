dda352b11969ed087a1945dccdf963fab8da1cfd
zeethebeast <chimasamuel6@gmail.com>
2024-11-01 02:58:05 +0100

    utf-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Validate if a given list of integers 
+represents a valid UTF-8 encoded string.
+Each integer represents a byte (0-255).
+"""
+
+def validUTF8(data):
+    num_byte = 0
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
+
+    for byte in data:
+        if num_byte == 0:
+            mask = 1 << 7
+            while mask & byte:
+                num_byte += 1
+                mask >>= 1
+
+            if num_byte == 0:
+                continue
+
+            if num_byte == 1 or num_byte > 4:
+                return False
+        else:
+            # Check if the byte starts with '10' in binary form
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+
+        num_byte -= 1
+
+    return num_byte == 0
