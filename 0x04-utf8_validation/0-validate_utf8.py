46939de1e11c4a7a170ef20dfc77db44eaeeb221
zeethebeast <chimasamuel6@gmail.com>
2024-11-01 03:33:43 +0100

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,22 +13,22 @@ def validUTF8(data):
         if byte < 0 or byte > 255:
             return False
 
-        # If no continuation bytes are expected
+        # If no continuation bytes are expected, identify the type of byte
         if num_bytes == 0:
-            # Determine number of bytes in the UTF-8 character
-            if byte >> 5 == 0b110:  # 2-byte character
+            # Check lead byte to determine the number of bytes in the UTF-8 character
+            if (byte >> 5) == 0b110:  # 2-byte character
                 num_bytes = 1
-            elif byte >> 4 == 0b1110:  # 3-byte character
+            elif (byte >> 4) == 0b1110:  # 3-byte character
                 num_bytes = 2
-            elif byte >> 3 == 0b11110:  # 4-byte character
+            elif (byte >> 3) == 0b11110:  # 4-byte character
                 num_bytes = 3
-            elif byte >> 7:  # If top bit is 1 but not a valid lead byte
+            elif (byte >> 7):  # If it starts with '1' but isn't a valid lead byte
                 return False
         else:
-            # Check continuation byte starts with '10'
-            if byte >> 6 != 0b10:
+            # Continuation byte check: should start with '10'
+            if (byte >> 6) != 0b10:
                 return False
             num_bytes -= 1
 
-    # All characters processed successfully
+    # If there are unfulfilled continuation bytes, it's invalid
     return num_bytes == 0
